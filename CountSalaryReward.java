b280290d5da629aaf45bd400728b3a50de52575e
IngaSu <inga.supranovich@gmail.com>
2018-10-15 10:43:13 +0300

    Hometask_5

---
+++ CountSalaryReward.java
@@ -0,0 +1,14 @@
+package task5_1;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class CountSalaryReward {
+
+	public static boolean main(String[] args) {
+		System.out.println(Tester.countReward(2400));			
+		}
+		
+	}
+
+}
